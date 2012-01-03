32594941e453394e0b7108bf84965a51a294e3bc
Cox Chen <coxchen@gmail.com>
2012-01-04 00:07:49 +0800

    1. do some refactorings; 2. highlight difference when comparing two traceback paths

--- py/cs_xml_report_compare.py
+++ py/cs_xml_report_compare.py
@@ -8,12 +8,15 @@
 #  Email: cox.chen@armorize.com
 #
 
-import sys, hashlib
+import sys, hashlib, time
+from timeit import Timer
 from difflib import SequenceMatcher as seqmatcher
 from BeautifulSoup import BeautifulStoneSoup as bs
 
 SEPARATOR_LENGTH = 43
 CSI = "\x1B["
+HIGH_COLOR = "35m"
+CLOSE_COLOR = "0m"
 
 class XML_Report_Comparator:
     def __init__(self, oldSoup, newSoup):
@@ -35,9 +38,9 @@ class XML_Report_Comparator:
         self.newVulnCount = len(newSoup.findAll('rd:vulnerability'))
         
         self.stats = {}
-        self.build_stats()
+        self.build_stats2()
     
-    def build_stats(self):
+    def _empty_stats(self):
         for vulnType in self.abstract['DELETE'].keys():
             self.stats[vulnType] = {'MATCHED': 0, 'INSERT': 0, 'DELETE': 0}
             self.diffAbstract['DELETE'][vulnType] = {}
@@ -47,34 +50,58 @@ class XML_Report_Comparator:
             if vulnType not in self.stats.keys():
                 self.stats[vulnType] = {'MATCHED': 0, 'INSERT': 0, 'DELETE': 0}
                 self.diffAbstract['INSERT'][vulnType] = {}
-        
-        matchedCount = 0
-        insertCount = 0
-        deleteCount = 0
+    
+    def build_stats2(self):
+        self._empty_stats()
         
         for vulnType in self.abstract['INSERT'].keys():
             if self.abstract['DELETE'].has_key(vulnType):
                 self.newCount += len(self.abstract['INSERT'][vulnType].keys())
                 self.oldCount += len(self.abstract['DELETE'][vulnType].keys())
                 
-                for vulnKey in self.abstract['INSERT'][vulnType].keys():
-                    if self.abstract['DELETE'][vulnType].has_key(vulnKey):
-                        self.abstract['DELETE'][vulnType][vulnKey]['status'] = 'MATCHED'
-                        self.abstract['INSERT'][vulnType][vulnKey]['status'] = 'MATCHED'
-                        self.stats[vulnType]['MATCHED'] += 1
-                        matchedCount += 1
-                    else:
-                        self.abstract['INSERT'][vulnType][vulnKey]['status'] = 'INSERT'
-                        self.diffAbstract['INSERT'][vulnType][vulnKey] = self.abstract['INSERT'][vulnType][vulnKey]
+                newVulnKeys = self.abstract['INSERT'][vulnType].keys()
+                oldVulnKeys = self.abstract['DELETE'][vulnType].keys()
+                
+                matchedKeys = set(oldVulnKeys).intersection(set(newVulnKeys))
+                for matchedKey in matchedKeys:
+                    self.abstract['INSERT'][vulnType][matchedKey]['status'] = 'MATCHED'
+                    self.abstract['DELETE'][vulnType][matchedKey]['status'] = 'MATCHED'
+                    self.stats[vulnType]['MATCHED'] += 1
+                
+                for key in self.abstract['INSERT'][vulnType].keys():
+                    if self.abstract['INSERT'][vulnType][key]['status'] != 'MATCHED':
+                        self.abstract['INSERT'][vulnType][key]['status'] = 'INSERT'
+                        self.diffAbstract['INSERT'][vulnType][key] = self.abstract['INSERT'][vulnType][key]
                         self.stats[vulnType]['INSERT'] += 1
-                        insertCount += 1
+                for key in self.abstract['DELETE'][vulnType].keys():
+                    if self.abstract['DELETE'][vulnType][key]['status'] != 'MATCHED':
+                        self.abstract['DELETE'][vulnType][key]['status'] = 'DELETE'
+                        self.diffAbstract['DELETE'][vulnType][key] = self.abstract['DELETE'][vulnType][key]
+                        self.stats[vulnType]['DELETE'] += 1
+    
+    def build_stats(self):
+        self._empty_stats()
+        
+        for vulnType in self.abstract['INSERT'].keys():
+            if self.abstract['DELETE'].has_key(vulnType):
+                self.newCount += len(self.abstract['INSERT'][vulnType].keys())
+                self.oldCount += len(self.abstract['DELETE'][vulnType].keys())
+                
+                for key in self.abstract['INSERT'][vulnType].keys():
+                    if self.abstract['DELETE'][vulnType].has_key(key):
+                        self.abstract['DELETE'][vulnType][key]['status'] = 'MATCHED'
+                        self.abstract['INSERT'][vulnType][key]['status'] = 'MATCHED'
+                        self.stats[vulnType]['MATCHED'] += 1
+                    else:
+                        self.abstract['INSERT'][vulnType][key]['status'] = 'INSERT'
+                        self.diffAbstract['INSERT'][vulnType][key] = self.abstract['INSERT'][vulnType][key]
+                        self.stats[vulnType]['INSERT'] += 1
                 
                 deleteVulnKeys = filter(lambda x : self.abstract['DELETE'][vulnType][x]['status'] != 'MATCHED', self.abstract['DELETE'][vulnType].keys())
                 self.stats[vulnType]['DELETE'] = len(deleteVulnKeys)
-                deleteCount += len(deleteVulnKeys)
-                for deleteVulnKey in deleteVulnKeys:
-                    self.abstract['DELETE'][vulnType][deleteVulnKey]['status'] = 'DELETE'
-                    self.diffAbstract['DELETE'][vulnType][deleteVulnKey] = self.abstract['DELETE'][vulnType][deleteVulnKey]
+                for key in deleteVulnKeys:
+                    self.abstract['DELETE'][vulnType][key]['status'] = 'DELETE'
+                    self.diffAbstract['DELETE'][vulnType][key] = self.abstract['DELETE'][vulnType][key]
     
     def print_stats(self):
         print '-' * SEPARATOR_LENGTH
@@ -82,7 +109,7 @@ class XML_Report_Comparator:
         print 'NEW:', self.newVulnCount, ',', self.newCount, 'unique paths'
         print
         
-        print ' IDX | {0:^15} | MAT | INS | DEL |'.format('V Type')
+        print ' IDX | {:^15} | MAT | INS | DEL |'.format('V Type')
         print '-' * SEPARATOR_LENGTH
         for vulnType in self.stats.keys():
             print '{idx:^5}| {vType:>15} | {match:^3} | {ins:^3} | {delete:^3} |'.format(idx = self.stats.keys().index(vulnType) + 1, vType = vulnType, match = self.stats[vulnType]['MATCHED'], ins = self.stats[vulnType]['INSERT'], delete = self.stats[vulnType]['DELETE'])
@@ -109,21 +136,22 @@ class XML_Report_Comparator:
         vulnCount = 0
         if self.stats[vulnType]['INSERT'] > 0: 
             print '\n INS: Newly found vulnerabilities'
-            print '-' * 30
+            print '-' * SEPARATOR_LENGTH
             for abstract in self.diffAbstract['INSERT'][vulnType].values():
                 if abstract['status'] != 'MATCHED':
                     vulnCount += 1
-                    print ' #{count}'.format(count = vulnCount), abstract['tb']
+                    print ' [{count}]'.format(count = vulnCount), abstract['tb']
         print
         if self.stats[vulnType]['DELETE'] > 0:
             print ' DEL: Disappeared vulnerabilities'
-            print '-' * 30
+            print '-' * SEPARATOR_LENGTH
             for abstract in self.diffAbstract['DELETE'][vulnType].values():
                 if abstract['status'] != 'MATCHED':
                     vulnCount += 1
-                    print ' #{count}'.format(count = vulnCount), abstract['tb']
+                    print ' [{count}]'.format(count = vulnCount), abstract['tb']
         print '\n {vType} | INSERT:{ins:^3} | DELETE:{delete:^3} |\n'.format(vType = vulnType, ins = self.stats[vulnType]['INSERT'], delete = self.stats[vulnType]['DELETE'])
-        print ' => Enter vulnerability index (1 - {count}) to view the traceback:'.format(count = vulnCount)
+        if vulnCount > 0:
+            print ' => Enter vulnerability index (1 - {count}) to view the traceback:'.format(count = vulnCount)
         print ' => Press ANY KEY ELSE to continue:',
         userInput = raw_input().upper()
         
@@ -142,33 +170,70 @@ class XML_Report_Comparator:
         self.show_diff(vulnType)
     
     def show_vuln(self, vulnType, catalog, vulnIdx):
-        vulnAbstract = self.diffAbstract[catalog][vulnType].values()[int(vulnIdx) - 1]
-        uuid = vulnAbstract['uuid']
-        vuln = self.soup[catalog].findAll('rd:vulnerability', uuid = uuid)[0]
+        theVuln = self.diffAbstract[catalog][vulnType].values()[int(vulnIdx) - 1]
         
-        print '-' * 30
-        print 'UUID:' + uuid
-        print '-' * 30
-        tbAbstract = vulnAbstract['tb']
-        print tbAbstract.replace(';', '\n')
+        idxBase = 0
+        if catalog == 'DELETE':
+            idxBase = len(self.diffAbstract['INSERT'][vulnType])
         
+        self.print_vuln_abstract_uuid(vulnIdx + idxBase, theVuln)
+        self.print_vuln_abstract_tb(theVuln)
+        
+        counterCatalog = filter(lambda x: x != catalog, self.diffAbstract.keys())[0]
+        counterIdxBase = 0
+        if counterCatalog == 'DELETE':
+            counterIdxBase = len(self.diffAbstract['INSERT'][vulnType])
+        
+        similarTBIdx = []
+        counterIdx = counterIdxBase
         print
-        counterCatalog = filter(lambda x: x!= catalog, [x for x in self.diffAbstract.keys()])[0]
-        if counterCatalog == 'INSERT':
-            simIdx = 0
-        else:
-            simIdx = len(self.diffAbstract['INSERT'][vulnType].keys())
-        for counterAbstract in self.diffAbstract[counterCatalog][vulnType].values():
-            simIdx += 1
-            similarity = seqmatcher(None, tbAbstract, counterAbstract['tb']).quick_ratio()
-            if similarity > 0.8:
-                print ' #{idx} {sim:.2f}'.format(idx = simIdx, sim = similarity), counterAbstract['uuid']
+        for counterVuln in self.diffAbstract[counterCatalog][vulnType].values():
+            counterIdx += 1
+            sMatcher = seqmatcher(None, theVuln['tb'], counterVuln['tb'])
+            if sMatcher.ratio() > 0.75:
+                similarTBIdx.append(str(counterIdx))
+                print '\t[{0}] {1:.1f}% {2}'.format(counterIdx, sMatcher.ratio() * 100, counterVuln['uuid'])
         
         print '\n  => Press d/D to show detailed traceback;'
-        print '  => ANY KEY ELSE to continue:',
+        if len(similarTBIdx) > 0:
+            print '  => enter index of similar vulnerability to compare;'
+        print '  => or enter ANY KEY ELSE to continue:',
         userInput = raw_input().upper()
+        
+        inputProcessed = False
         if userInput == 'D':
-            self.show_vuln_detail(vuln, catalog)
+            vulnDetail = self.soup[catalog].findAll('rd:vulnerability', uuid = theVuln['uuid'])[0]
+            self.show_vuln_detail(vulnDetail, catalog)
+            inputProcessed = True
+        elif userInput in similarTBIdx:
+            self.compare(vulnType, catalog, vulnIdx, counterCatalog, userInput)
+            inputProcessed = True
+        else:
+            return
+        self._press_any_key()
+        self.show_vuln(vulnType, catalog, vulnIdx)
+    
+    def print_vuln_abstract_uuid(self, vulnIdx, theVuln):
+        print '-' * SEPARATOR_LENGTH
+        print '[{0}] UUID: {1}'.format(vulnIdx, theVuln['uuid'])
+    
+    def print_vuln_abstract_tb(self, theVuln, diffOpcodes = [], opcodeOffset = 1):
+        print '-' * SEPARATOR_LENGTH
+        
+        if len(diffOpcodes) == 0:
+            print theVuln['tb'].replace(';', '\n')
+        else:
+            augmentedVulnAbstract = ''
+            for opcode in diffOpcodes:
+                hStart = opcode[opcodeOffset]
+                hEnd = opcode[opcodeOffset + 1]
+                if opcode[0] == 'equal':
+                    fragment = theVuln['tb'][hStart:hEnd]
+                    
+                else:
+                    fragment = CSI + HIGH_COLOR + theVuln['tb'][hStart:hEnd].replace(';', '\n') + CSI + CLOSE_COLOR
+                augmentedVulnAbstract += fragment
+            print augmentedVulnAbstract.replace(';', '\n')
             
     def show_vuln_detail(self, vuln, catalog):
         for step in vuln.findAll('rd:step'):
@@ -181,6 +246,28 @@ class XML_Report_Comparator:
             for snippet in step.findAll('rd:snippet'):
                 print '\t' * int(step['indent']), snippet.contents[0]
         print '-' * 30
+    
+    def compare(self, vulnType, oCatalog, oIdx, cCatalog, cIdx):
+        cIdxBase = 0
+        oIdxBase = 0
+        if cCatalog == 'DELETE':
+            cIdxBase = len(self.diffAbstract['INSERT'][vulnType])
+        else:
+            oIdxBase = len(self.diffAbstract['INSERT'][vulnType])
+        
+        print '-' * SEPARATOR_LENGTH
+        print 'Comparing {0}: {1}({2}) with {3}({4})'.format(vulnType, oIdx + oIdxBase, oCatalog, cIdx, cCatalog)
+        
+        oVuln = self.diffAbstract[oCatalog][vulnType].values()[int(oIdx) - 1]
+        cVuln = self.diffAbstract[cCatalog][vulnType].values()[int(cIdx) - cIdxBase - 1]
+        
+        s = seqmatcher(None, oVuln['tb'], cVuln['tb'])
+        self.print_vuln_abstract_uuid(oIdx + oIdxBase, oVuln)
+        self.print_vuln_abstract_tb(oVuln, s.get_opcodes(), 1)
+        self.print_vuln_abstract_uuid(cIdx, cVuln)
+        self.print_vuln_abstract_tb(cVuln, s.get_opcodes(), 3)
+    
+    def _press_any_key(self):
         print '\n => Press ANY KEY to continue:',
         raw_input()
 
@@ -231,15 +318,22 @@ def extract_vulnerability_abstracts(aReportSoup):
     
     return vulnAbstractDict
 
-def main(oldXmlReport, newXmlReport):
+def create_comparator(oldSoup, newSoup):
+    comparator = XML_Report_Comparator(oldSoup, newSoup)
+    return comparator
+
+def main():
+    oldXmlReport = sys.argv[1]
+    newXmlReport = sys.argv[2]
     oldSoup = bs(file(oldXmlReport).read(), convertEntities=bs.HTML_ENTITIES)
     newSoup = bs(file(newXmlReport).read(), convertEntities=bs.HTML_ENTITIES)
     
-    comparator = XML_Report_Comparator(oldSoup, newSoup)
-    comparator.show_options()
+    begin = time.time()
+    comparator = create_comparator(oldSoup, newSoup)
+    end = time.time()
+    print '\n# took {0:.2f} secs to build the stats ...\n'.format(end - begin)
     
+    comparator.show_options()
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
-    
-    #main('gg301_r33648.xml', 'gg301_r33729.xml')
+    main()
